98cb4ec82f1d500bf19a643b21579b7eb333e796
Karl von Randow <karl@xk72.com>
2019-01-20 22:41:08 +1300

    build.sh: add command to get logspout-gelf source

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ cp -r /src /go/src/github.com/gliderlabs/logspout
 cd /go/src/github.com/gliderlabs/logspout
 export GOPATH=/go
 go get github.com/Masterminds/glide && $GOPATH/bin/glide install
+go get github.com/micahhausler/logspout-gelf
 go build -ldflags "-X main.Version=$1" -o /bin/logspout
 apk del go git mercurial build-base
 rm -rf /go /var/cache/apk/* /root/.glide
